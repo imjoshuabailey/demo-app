4443121e9afa8f4b119d120d5977370e93759627
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-02-17 19:59:43 -0800

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewTesterComponent } from './new-tester/new-tester.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTesterComponent
   ],
   imports: [
     BrowserModule,
